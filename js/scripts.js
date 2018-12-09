42a35b4be54237c2ed2b25f7cda14594660b03d1
antoni silva rojas <antony_grone12@live.com>
2018-12-08 20:54:28 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+function mostrar_ocultar(id){
+    if(document.getElementById){
+        var el=document.getElementById(id);
+        el.style.display=(el.style.display=='none') ? 
+        'block':'none';
+
+        el.style.display=(el.style.display=='none') ? 
+        document.getElementById('agregar').className='fa fa-chevron-down':document.getElementById('agregar').className='fa fa-chevron-up';
+        // document.getElementById('admin').style.display='block';
+        // document.getElementById('agregar').className='fa fa-chevron-up';
+    }
+}
+
+function mostrar_ocultar_menu_reportes(id1){
+    if(document.getElementById){
+        var el1=document.getElementById(id1);
+        el1.style.display=(el1.style.display=='none') ? 
+        'block':'none';
+
+        el1.style.display=(el1.style.display=='none') ? 
+        document.getElementById('bajar').className='fa fa-chevron-down':document.getElementById('bajar').className='fa fa-chevron-up';
+    }
+}
+
+// document.getElementById('other-menu').ondblclick=function(){
+//     mostrar_ocultar_menu_reportes('submenu-reportes1');
+// }
